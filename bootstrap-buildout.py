6a2cd12df2a6e54e88c24420dc9086a732aafc5c
Pilar Marinas <pilar.marinas@upcnet.es>
2025-01-14 14:34:11 +0100

    [PY3] 2to3

--- bootstrap-buildout.py
+++ bootstrap-buildout.py
@@ -18,6 +18,11 @@ The script accepts buildout command-line options, so you can
 use the -c option to specify an alternate configuration file.
 """
 
+from urllib.request import urlopen
+import zc.buildout.buildout
+import subprocess
+import pkg_resources
+import setuptools
 import os
 import shutil
 import sys
@@ -73,17 +78,12 @@ parser.add_option("--setuptools-to-dir",
 
 options, args = parser.parse_args()
 if options.version:
-    print("bootstrap.py version %s" % __version__)
+    print(("bootstrap.py version %s" % __version__))
     sys.exit(0)
 
 
 ######################################################################
 # load/install setuptools
-
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import urlopen
 
 ez = {}
 if os.path.exists('ez_setup.py'):
@@ -115,8 +115,6 @@ if options.setuptools_to_dir is not None:
     setup_args['to_dir'] = options.setuptools_to_dir
 
 ez['use_setuptools'](**setup_args)
-import setuptools
-import pkg_resources
 
 # This does not (always?) update the default working set.  We will
 # do it.
@@ -143,7 +141,7 @@ find_links = os.environ.get(
     options.find_links or
     ('http://downloads.buildout.org/'
      if options.accept_buildout_test_releases else None)
-    )
+)
 if find_links:
     cmd.extend(['-f', find_links])
 
@@ -187,7 +185,6 @@ if version:
     requirement = '=='.join((requirement, version))
 cmd.append(requirement)
 
-import subprocess
 if subprocess.call(cmd) != 0:
     raise Exception(
         "Failed to execute command:\n%s" % repr(cmd)[1:-1])
@@ -197,7 +194,6 @@ if subprocess.call(cmd) != 0:
 
 ws.add_entry(tmpeggs)
 ws.require(requirement)
-import zc.buildout.buildout
 
 if not [a for a in args if '=' not in a]:
     args.append('bootstrap')
